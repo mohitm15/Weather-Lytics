edbd7899a6871ad7d2c6b417d6ff810a25f55a94
Mohit Maroliya <mmaroliya@gmail.com>
2022-03-19 17:00:01 +0530

    Setting Up Project Files

---
+++ pages/api/today.js
@@ -0,0 +1,15 @@
+
+const apikey = process.env.REACT_APP_API_KEY_1;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Bhopal&appid=${apikey}`;
+
+const fecthInfo = async() => {
+    const response = await fetch(url);
+    const data = await response.json() ;
+    return data;
+}
+export default function handler(req, res) {
+    const result = fecthInfo();
+    console.log(result)
+    res.status(200).json(result)
+  }
+  
---
+++ pages/components/Weather_Today.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Weather_Today = (props) => {
+
+    
+  return (
+    <>
+    <div>
+    Weather_Today
+          <br/>City name, state code and country code divided by comma, Please, refer
+          to ISO 3166 for the state codes or country codes. You can specify the
+          parameter not only in English. In this case, the API response should
+          be returned in the same language as the language of requested location
+          name if the mohit mihti mohit mohti mpohti
+          name if the location is in our predefined list of more than 200,000
+          locations.
+          </div>
+    </>
+  )
+}
+
+export default Weather_Today;
